d154c4f394725713b9724e204ba8253ccccb52fa
wilsonturpo <wilson.turpo@tecsup.edu.pe>
2021-05-21 15:04:25 -0500

    Google fonts y PhotoView

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:google_fonts/google_fonts.dart';
+//import 'package:animated_text_kit/animated_text_kit.dart';
+// import 'package:google_fonts/google_fonts.dart';
+import 'package:photo_view/photo_view.dart';
 
 
 void main() {
@@ -61,67 +62,12 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Google Fonts: Goblin One',
-              style: GoogleFonts.getFont('Goblin One', fontSize: 20),            
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                const SizedBox(width: 20.0, height: 100.0),
-                const Text(
-                  'Be',
-                  style: 
-                  TextStyle(fontSize: 43.0),        
-                ),
-                const SizedBox(width: 20.0, height: 100.0),
-                DefaultTextStyle(
-                  style: const TextStyle(
-                    fontSize: 40.0,
-                    fontFamily: 'Horizon',
-                  ),
-                  child: AnimatedTextKit(
-                    animatedTexts: [
-                      RotateAnimatedText('AWESOME'),
-                      RotateAnimatedText('OPTIMISTIC'),
-                      RotateAnimatedText('DIFFERENT'),
-                    ]
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body:PhotoView(
+            imageProvider: AssetImage("assets/img/tecsup.jpg"),
+          )
     );
   }
 }
